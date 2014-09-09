167d24ba9394f974b89d47be75fc7a77088920f4
Alexandre Girard <git@alexgirard.com>
2014-09-09 18:02:25 +0200

    first commit

---
+++ GeoInfo.php
@@ -0,0 +1,26 @@
+<?php
+class RawSource {
+
+    static function Load() {
+    }
+
+    static function Info() {
+        return array (
+            'name'    => 'GeoInfo',
+            'author'  => 'Alexandre Girard',
+            'version' => '1.0.0',
+            'site'    => 'http://alexgirard.com/',
+            'notes'   => 'Add geographic information (latitude/longitude) for each video'
+        );
+    }
+
+    static function Install() {
+    }
+
+    static function Uninstall() {
+    }
+
+    static function Settings() {
+    }
+}
+?>
---
+++ README.md
@@ -0,0 +1,10 @@
+CumulusClips Plugin : GeoInfo
+=============================
+
+Add geographic information (latitude/longitude) for each video.
+
+# Install
+
+1. Go to your CumulusClips plugin folder
+2. ```git clone https://github.com/alx/GeoInfo.git```
+3. In CumulusClips admin interface, enable GeoInfo plugin from plugins panel
