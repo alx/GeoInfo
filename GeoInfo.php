e9f933e92078560a9fff12770caf472b89b3a06b
Alexandre Girard <git@alexgirard.com>
2014-11-24 09:57:25 +0100

    correct syntax

--- GeoInfo.php
+++ GeoInfo.php
@@ -182,7 +182,7 @@ $(document).ready(function(){
           </form>
         </td>
         <td class="video-map">
-          <?php if(isset($geoinfo) && intval($lat) != 0 && intval($long) != 0) { ?>
+          <?php if(isset($geoinfo) && (intval($lat) != 0 || intval($long) != 0)) { ?>
           <img border="0" alt="Marker of video <?= $video->title ?>" src="<?= GeoInfo::MapUrl($geoinfo->geoinfo_id) ?>"></img>
           <?php } else { ?>
           <p>Set geoinfo to display map</p>
